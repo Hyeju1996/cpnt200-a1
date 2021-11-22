3ba099490a6ccf147c5bfd647c72496c6f11b9fb
Hyeju <hyeju1996@hotmail.com>
2021-11-21 20:06:03 -0800

    first do nuxt

---
+++ pages/images.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img :src="image" />
+  </div>
+</template>
+
+<script>
+import image from "./assets/logo.png";
+
+export default {
+  data: function () {
+    return {
+      image: image,
+    };
+  },
+};
+</script>
